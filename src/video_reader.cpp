7a0e96a1566595514ae3c276b350758da549420e
thefloppydriver <thefloppydriver@gmail.com>
2021-10-02 12:57:46 -0400

    Fixes gcc depreciation
    changes fix av_err2str taking address of temporary array and add required compilation flags to CMakeLists.txt

--- src/video_reader.cpp
+++ src/video_reader.cpp
@@ -3,6 +3,18 @@ extern "C" {
 #include <libavdevice/avdevice.h>
 }
 
+#ifdef av_err2str
+#undef av_err2str
+#include <string>
+int av_strerror(int errnum, char *errbuf, size_t errbuf_size);
+
+av_always_inline std::string av_err2string(int errnum) {
+    char str[AV_ERROR_MAX_STRING_SIZE];
+    return av_make_error_string(str, AV_ERROR_MAX_STRING_SIZE, errnum);
+}
+#define av_err2str(err) av_err2string(err).c_str()
+#endif  // av_err2str
+
 bool video_reader_open(VideoReaderState* state, const char* filename) {
 
     avdevice_register_all();
